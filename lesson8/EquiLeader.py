ff94d68a03dc4c6858ed3aae6729dc5f1c296488
Daniel Phillips <dphildev@gmail.com>
2018-11-07 21:24:21 +0000

    This one was particularly painful for some reason. Finally got there though!

--- lesson8/EquiLeader.py
+++ lesson8/EquiLeader.py
@@ -13,18 +13,46 @@ def find_leaders(A):
     if count == 0:
         return -1, []
         
-    leader_positions = []
+    leader_counts = [0] * len(A)
+    leader_count = 0
     for i in range(len(A)):
         if A[i] == potential_leader:
-            leader_positions.append(i)
+            leader_count += 1
+        leader_counts[i] = leader_count
     
-    if len(leader_positions) > len(A) // 2:
+    if leader_count > len(A) // 2:
         # found a leader!
-        return potential_leader, leader_positions
+        return potential_leader, leader_counts
     else:
         return -1, []
         
 def solution(A):
-    print(find_leaders(A))
+    leader, leader_counts = find_leaders(A)
+    if not leader_counts:
+        return 0
+        
+    total_leaders = 0
+    #print('Leader counts: {}'.format(leader_counts))
     
-    pass
+    # skip the last element- we still need a right hand side to find an equileader
+    for i in range(len(A) - 1):
+        left_leaders = leader_counts[i]
+        right_leaders = leader_counts[-1] - left_leaders
+        #print('left_leaders: {} right_leaders: {}'.format(left_leaders, right_leaders))
+        
+        left_elements = i + 1
+        right_elements = len(A) - left_elements
+        #print('left_elements: {} right_elements: {}'.format(left_elements, right_elements))
+        
+        left_required = left_elements // 2
+        right_required = right_elements // 2
+        #print('left_required: {} right_required: {}'.format(left_required, right_required))
+        
+        sufficient_leaders_on_left = left_leaders > left_required
+        sufficient_leaders_on_right = right_leaders > right_required
+        #print('Left: {} Right: {}'.format(sufficient_leaders_on_left, sufficient_leaders_on_right))
+        
+        if sufficient_leaders_on_left and sufficient_leaders_on_right:
+            total_leaders += 1
+    
+    return total_leaders

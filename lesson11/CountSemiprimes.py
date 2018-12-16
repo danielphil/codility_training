27018eab5d9d644d22e4075d6981786a601dd28c
Daniel Phillips <dphildev@gmail.com>
2018-12-16 18:06:47 +0000

    Give up after we find more than two prime factors (therefore making the number not semiprime) as that was a lot of work we didn't need to do, then use a partial sum to make calculating the ranges at the end faster. Now it gets 100% for everything!

--- lesson11/CountSemiprimes.py
+++ lesson11/CountSemiprimes.py
@@ -13,36 +13,36 @@ def smallest_factors(N):
         i += 1
     
     return factors
-    
+
+def generate_sums(semiprimes):
+    partial_sums = [0] * (len(semiprimes) + 1)
+    for i in range(1, len(semiprimes)):
+        partial_sums[i] = semiprimes[i] + partial_sums[i - 1]
+    return partial_sums
+
 def solution(N, P, Q):
     # perhaps partial sums for performance?
     factors = smallest_factors(N)
-    semiprimes = [False] * (N + 1)
+    semiprimes = [0] * (N + 1)
     start = min(P)
     end = max(Q)
     for i in range(start, end + 1):
         # if the number is not prime, find the list of divisors
         if factors[i] != 0:
             divisor = factors[i]
-            val = i
-            current_factors = []
-            while factors[val] != 0:
-                val = val // divisor
-                current_factors += [val]
-            current_factors += [divisor]
-            if (len(current_factors) == 2):
-                semiprimes[i] = True
-    
+            # we can only find one more prime factor for the number to be classed
+            # as semiprime
+            if factors[i // divisor] == 0:
+                semiprimes[i] = 1
+
     #print([i for i in range(len(semiprimes)) if semiprimes[i]])
+    
+    sums = generate_sums(semiprimes)
     
     results = []
     for i in range(len(P)):
         start = P[i]
         end = Q[i]
-        count = 0
-        for j in range(start, end + 1):
-            if semiprimes[j]:
-                count += 1
-        results.append(count)
-        
+        results.append(sums[end] - sums[start - 1])
+
     return results

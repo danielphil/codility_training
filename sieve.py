2d21d5bf9e04abe48e2a93bd349539c0e263d5d5
Daniel Phillips <dphildev@gmail.com>
2018-12-16 12:42:09 +0000

    Adding factorisation example.

--- sieve.py
+++ sieve.py
@@ -1,4 +1,6 @@
 import unittest
+from functools import reduce
+
 # based on Codility's notes on Sieve of Eratosthenes
 def sieve(n):
     # create a list where prime numbers are marked with True, and non-prime 
@@ -24,12 +26,53 @@ def sieve(n):
     # just the primes!
     return [p for p in range(n + 1) if sieve[p] == True]
 
+def get_factors(n):
+    factors = [0] * (n + 1)
+    i = 2
+
+    while i * i <= n:
+        if factors[i] == 0:
+            k = i * i
+            while k <= n:
+                if factors[k] == 0:
+                    factors[k] = i
+                k += i
+        
+        i += 1
+    
+    return factors
+
+def factorise(n):
+    factors = get_factors(n)
+    result = []
+
+    i = n
+    while factors[i] > 0:
+        result.append(factors[i])
+        i //= factors[i]
+    result.append(i)
+
+    return result
+
+def product(iterable):
+    return reduce(lambda x, y: x * y, iterable, 1)
+
 class Test(unittest.TestCase):
-    def test_example(self):
+    def test_sieve(self):
         primes = sieve(29)
         expected_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
         self.assertEqual(primes, expected_primes)
 
+    def test_get_factors(self):
+        factors = get_factors(20)
+        expected_factors = [0, 0, 0, 0, 2, 0, 2, 0, 2, 3, 2, 0, 2, 0, 2, 3, 2, 0, 2, 0, 2]
+        self.assertEqual(factors, expected_factors)
+
+    def test_factorise(self):
+        factors = factorise(2)
+        expected_factors = [2, 2, 5]
+        self.assertEqual(factors, expected_factors)
+        self.assertEqual(product(expected_factors), 20)
 
 if __name__ == "__main__":
     unittest.main()

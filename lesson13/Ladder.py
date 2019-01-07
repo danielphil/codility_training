f9771389211e376f853dcfda84312627cbf5a8d8
Daniel Phillips <dphildev@gmail.com>
2019-01-07 22:38:34 +0000

    Now giving correct answers, but too slow in the extreme case. Now to optimise it.

--- lesson13/Ladder.py
+++ lesson13/Ladder.py
@@ -1,18 +1,13 @@
 def solution(A, B):
-    result = []
+    fib = [0] * 50002
+    fib[1] = 1
     
+    for i in range(2, 50002):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    
+    result = []
     for a, b in zip(A, B):
-        count = 0
-        p = 2 ** b
-        
-        for i in range(1, a // 2 + 1):
-            count += a - i
-            count %= p
-            
-        if a % 2 == 1:
-            count += 1
-            count %= p
-            
-        result.append(count)
+        result.append(fib[a + 1] % (2 ** b))
         
     return result
+    

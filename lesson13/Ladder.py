f99cca92adc47cf8be63febcd64f14d9702ac443
Daniel Phillips <dphildev@gmail.com>
2019-01-07 22:58:14 +0000

    Finally got to 100%. The long integer seemed to be what wrecked the performance.

--- lesson13/Ladder.py
+++ lesson13/Ladder.py
@@ -1,13 +1,15 @@
 def solution(A, B):
-    fib = [0] * 50002
+    max_a = max(A)
+    max_b = max(B)
+
+    fib = [0] * (max_a + 2)
     fib[1] = 1
     
-    for i in range(2, 50002):
-        fib[i] = fib[i - 1] + fib[i - 2]
+    for i in range(2, max_a + 2):
+        fib[i] = (fib[i - 1] + fib[i - 2]) % (2 ** max_b)
     
-    result = []
-    for a, b in zip(A, B):
-        result.append(fib[a + 1] % (2 ** b))
+    result = [0] * len(A)
+    for i in range(len(A)):
+        result[i] = fib[A[i] + 1] % (2 ** B[i])
         
     return result
-    

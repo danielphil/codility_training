7aa57b6ed05ecae56aa7ee5f4f76ca55d0b369d5
Daniel Phillips <dphildev@gmail.com>
2019-01-19 11:23:26 +0000

    Playing with using a set to reduce the number of iterations. Still not good enough yet!

--- lesson14/NailingPlanks.py
+++ lesson14/NailingPlanks.py
@@ -1,17 +1,18 @@
 def validate(A, B, C, nails_to_use):
     planks = len(A)
-    nailed = [False] * planks
-    still_to_nail = planks
+    to_nail = set(range(planks))
 
-    
     for nail_index in range(nails_to_use):
         nail = C[nail_index]
-        for i in range(planks):
-            if not nailed[i] and A[i] <= nail <= B[i]:
-                nailed[i] = True
-                still_to_nail -= 1
+        to_remove = []
+        for i in to_nail:
+            if A[i] <= nail <= B[i]:
+                to_remove.append(i)
 
-        if still_to_nail == 0:
+        for index in to_remove:
+            to_nail.remove(index)
+            
+        if len(to_nail) == 0:
             # all planks nailed
             return True
     

116b05a74968d35c7c17d2b9de0ef7c22aade157
Daniel Phillips <dphildev@gmail.com>
2019-01-19 11:16:40 +0000

    Performance no better yet.

--- lesson14/NailingPlanks.py
+++ lesson14/NailingPlanks.py
@@ -1,10 +1,10 @@
-def solution(A, B, C):
+def validate(A, B, C, nails_to_use):
     planks = len(A)
     nailed = [False] * planks
     still_to_nail = planks
+
     
-    # iterate through all the nails
-    for nail_index in range(len(C)):
+    for nail_index in range(nails_to_use):
         nail = C[nail_index]
         for i in range(planks):
             if not nailed[i] and A[i] <= nail <= B[i]:
@@ -13,6 +13,23 @@ def solution(A, B, C):
 
         if still_to_nail == 0:
             # all planks nailed
-            return nail_index + 1 
-        
-    return -1
+            return True
+    
+    return False
+
+def solution(A, B, C):
+    # binary search through all the nails
+    lower_bound = 1
+    upper_bound = len(C)
+    
+    while lower_bound != upper_bound:
+        mid = (lower_bound + upper_bound) // 2
+        if validate(A, B, C, mid):
+            upper_bound = mid
+        else:
+            lower_bound = mid + 1
+
+    if validate(A, B, C, lower_bound):
+        return lower_bound
+    else:
+        return -1

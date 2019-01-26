2ca0f999b51d293abe674a2fa17837060b0cebee
Daniel Phillips <dphildev@gmail.com>
2019-01-26 20:20:52 +0000

    Finally got 100% on this. Need two loops, but can share iterations of r across iterations of q. Gets O(N ** 2) performance and 100%. Thanks again to the Internet for the tips on how to properly solve this one.

--- lesson15/CountTriangles.py
+++ lesson15/CountTriangles.py
@@ -1,3 +1,5 @@
+import random
+
 def solution(A):
     count = 0
     for p in range(len(A)):
@@ -10,3 +12,38 @@ def solution(A):
                     count += 1
     
     return count
+
+# test [10, 2, 5, 1, 8, 12] 
+
+def new_solution(A):
+    # need 3 elements to make this work
+    if len(A) < 3:
+        return 0
+
+    # sort first
+    count = 0
+    A.sort()
+    
+    for p_index in range(len(A) - 2):
+        # we can share r_index across iterations of q.
+        # if A[p] + A[q] > A[r], then A[p] + A[q + 1] > A[r]
+        r_index = p_index + 2
+        for q_index in range(p_index + 1, len(A) - 1):
+            p = A[p_index]
+            q = A[q_index]
+            # keep advancing r_index until we find a value that fails to satisfy p + q > r
+            while r_index < len(A) - 1 and p + q > A[r_index + 1]:
+                r_index += 1
+
+            if p + q > A[r_index]:
+                count += r_index - q_index
+
+    return count
+
+if __name__ == "__main__":
+    for i in range(100):
+        A = [random.randint(1,60) for p in range(20)]
+        good = solution(A)
+        new = new_solution(A)
+        if good != new:
+            print('Good: {} New: {} A: {}'.format(good, new, A))
